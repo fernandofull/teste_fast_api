1d070a0f742d8080a3d279564535986ed873b0d1
fernando <fernandoribeiro.ads@gmail.com>
2025-04-28 18:18:47 -0300

    primeiro commit

---
+++ main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from typing import Dict
+import uvicorn
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+# Simulando uma base de dados
+items_db = {
+    1: {"nome": "Notebook", "preço": 3500},
+    2: {"nome": "Mouse", "preço": 120},
+    3: {"nome": "Teclado", "preço": 200}
+}
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse(
+        "items.html",
+        {"request": request, "items": items_db}
+    )
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int) -> Dict:
+    if item_id not in items_db:
+        raise HTTPException(status_code=404, detail="Item não encontrado")
+    return items_db[item_id]
+
